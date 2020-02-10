8369c1743d2540da3bd74554cb44c6d49ed3b688
Ryan Lewien <rlewien3@gmail.com>
2020-02-10 23:40:30 +1100

    Version 1.0 running smoothly! Basic framework complete, including mobile view. All resources are still just stored locally.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Ryan Lewien
+Ryan Lewien's Personal Website
 
-This is a test.
+Version 1.0: Currently does not have recent projects on it, but a basic framework is there! Very excited about this page and where it'll go over time.
---
+++ js/scripts.js
@@ -0,0 +1,118 @@
+/* Click on nav links to scroll to them */
+$("#aboutlink").click(function() {
+$('html, body').animate({
+    scrollTop: $("#about").offset().top + 190
+}, 1000);
+});
+
+$("#mobile-aboutlink").click(function() {
+$('html, body').animate({
+    scrollTop: $("#about").offset().top + 190
+}, 1000);
+});
+
+$("#down-arrow").click(function() {
+$('html, body').animate({
+    scrollTop: $("#about").offset().top + 190
+}, 1000);
+});
+
+$("#what-i-dolink").click(function() {
+$('html, body').animate({
+    scrollTop: $("#what-i-do").offset().top + 100
+}, 1000);
+});
+
+$("#mobile-what-i-dolink").click(function() {
+$('html, body').animate({
+    scrollTop: $("#what-i-do").offset().top + 200
+}, 1000);
+});
+
+$("#projectslink").click(function() {
+$('html, body').animate({
+    scrollTop: $("#projects").offset().top + 190
+}, 1000);
+});
+
+$("#mobile-projectslink").click(function() {
+$('html, body').animate({
+    scrollTop: $("#projects").offset().top + 190
+}, 1000);
+});
+
+$("#mobile-contactlink").click(function() {
+$('html, body').animate({
+    scrollTop: $("#contact").offset().top + 190
+}, 1000);
+});
+
+
+/* Come in on scroll */
+(function($) {
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+  
+    $.fn.visible = function(partial) {
+      
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+      
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  
+    };
+      
+  })(jQuery);
+
+var win = $(window);
+var allSections = $("section");
+
+// Already visible modules
+allSections.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  allSections.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+});
+
+/* Close mobile nav when clicked */
+$(".mobile-link").click(function(){
+    $("#toggle").prop("checked", false);
+});
+
+/* Move floating icons when bottom reached */
+function checkOffset() {
+    var a=$(document).scrollTop()+window.innerHeight;
+    var b=$('footer').offset().top + 20;
+    if (a<b) {
+      $('.floating-icons').css('bottom', '55px');
+    } else {
+      $('.floating-icons').css('bottom', (55+(a-b))+'px');
+    }
+  }
+  $(document).ready(checkOffset);
+  $(document).scroll(checkOffset);
